c5e071336830cd5fc62fbc005b47dbcd5ec9270b
frectonz <fraol0912@gmail.com>
2024-11-21 06:35:44 +0300

    chore: 0.0.2

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
           cargo-pgrx = import ./nix/pgrx.nix { inherit pkgs; };
 
           pname = "pglite-fusion";
-          version = "0.0.1";
+          version = "0.0.2";
 
           postgres = pkgs.dockerTools.pullImage {
             imageName = "postgres";
--- nix/build.nix
+++ nix/build.nix
@@ -42,7 +42,7 @@ let
 in
 pkgs.rustPlatform.buildRustPackage {
   pname = "pglite-fusion";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = pkgs.lib.cleanSourceWith {
     src = pkgs.lib.cleanSource ../.;
